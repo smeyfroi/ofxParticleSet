2cc16ea795cf15f11fd9f0b919cd791524a4c1ab
smeyfroi <steve.meyfroidt@gmail.com>
2025-06-08 11:02:41 +0100

    Use meshes, offload to update thread

--- src/ofxParticleSet.cpp
+++ src/ofxParticleSet.cpp
@@ -55,6 +55,10 @@ void Particle::update(const std::vector<Particle>& particles,
 ParticleSet::ParticleSet(float drawScale_)
 : drawScale { drawScale_ }
 {
+  pointMesh.setMode(OF_PRIMITIVE_POINTS);
+  pointMesh.enableColors();
+  lineMesh.setMode(OF_PRIMITIVE_LINES);
+  lineMesh.enableColors();
   createSpatialIndex();
   setThreadName("ParticleSet " + ofToString(this));
   startThread();
@@ -70,7 +74,8 @@ void ParticleSet::eraseDeadParticles() {
   auto it = std::remove_if(particles.begin(),
                            particles.end(),
                            [](Particle& p) { return (!p.isAlive()); });
-  size_t excessParticles = std::max(0L, (it - particles.begin()) - maxParticles);
+//  size_t excessParticles = std::max(0L, (it - particles.begin()) - maxParticles);
+  size_t excessParticles = 0;
   particles.erase(it, particles.end()); // the ones that are no longer alive
   particles.erase(particles.begin(), particles.begin() + excessParticles); // the earliest excess ones
 }
@@ -99,17 +104,18 @@ void ParticleSet::add(glm::vec2 position, glm::vec2 velocity, ofFloatColor color
 void ParticleSet::threadedFunction() {
   ParticleSetUpdate update;
   while (updates.receive(update)) {
-    lock();
     do {
       std::for_each(update.newParticleData.begin(),
                     update.newParticleData.end(),
                     [&] (auto& d) {
-        particles.emplace_back(d.position,
-                               d.velocity,
-                               d.spin,
-                               particleDrawRadius,
-                               d.color,
-                               ofRandom(maxParticleAge));
+        if (particles.size() <= maxParticles) {
+          particles.emplace_back(d.position,
+                                 d.velocity,
+                                 d.spin,
+                                 particleDrawRadius,
+                                 d.color,
+                                 ofRandom(maxParticleAge));
+        }
       });
     } while(updates.tryReceive(update));
     std::for_each(particles.begin(),
@@ -124,46 +130,71 @@ void ParticleSet::threadedFunction() {
     });
     eraseDeadParticles();
     createSpatialIndex();
-    unlock();
+    updateMeshes();
   }
 }
 
-void ParticleSet::draw() {
+void ParticleSet::updateMeshes() {
   float particleConnectionRadius2 = particleConnectionRadius * particleConnectionRadius * drawScale * drawScale;
   ofx::KDTree<glm::vec2>::SearchResults searchResults;
   searchResults.resize(10);
   
-  lock();
-  {
-    ofFill();
-    for (int i = 0; i < particles.size(); i++) {
-      Particle p = particles[i];
-      ofFloatColor c = p.color;
-      spatialIndexPtr->findPointsWithinRadius(p.position, particleConnectionRadius * drawScale, searchResults);
+  ofMesh newPointMesh;
+  newPointMesh.setMode(OF_PRIMITIVE_POINTS);
+  newPointMesh.enableColors();
+  ofMesh newLineMesh;
+  newLineMesh.setMode(OF_PRIMITIVE_LINES);
+  newLineMesh.enableColors();
+
+  for (int i = 0; i < particles.size(); i++) {
+    Particle p = particles[i];
+    spatialIndexPtr->findPointsWithinRadius(p.position, particleConnectionRadius * drawScale, searchResults);
+    if (strategy != STRATEGY_POINTS) {
+      // draw connection if other particles close
       for (const auto& searchResult: searchResults) {
         if (searchResult.first <= i) continue; // don't double-count particles
         const Particle& otherParticle = particles[searchResult.first];
         if (p.position == otherParticle.position) continue;
         float distanceSquared = searchResult.second;
         float distanceScale = distanceSquared / particleConnectionRadius2; // 0 close, 1 far
-        c.a = p.color.a * (1.0 - distanceScale) * ((float)p.lifetime / maxParticleAge);
-        ofSetColor(c);
-        ofSetLineWidth(2 * p.drawRadius * drawScale);
-        ofDrawLine(p.position, otherParticle.position);
-      }
-      if (searchResults.size() <= 1) {
-        //      c.a *= std::sqrt((float)p.lifetime / maxParticleAge);// * (glm::length2(p.velocity) / 5.0);
-        ofSetColor(c);
-        ofDrawCircle(p.position, p.drawRadius * drawScale);
+        newLineMesh.addVertex({ p.position, 0.0 });
+        ofFloatColor c = p.color;
+        c.a = p.color.a * (1.0 - distanceScale) * colourMultiplier;
+        newLineMesh.addColor(c);
+        newLineMesh.addVertex({ otherParticle.position, 0.0 });
+        c.r = otherParticle.color.r;
+        c.r = otherParticle.color.g;
+        c.r = otherParticle.color.b;
+        newLineMesh.addColor(c);
       }
     }
+    // draw point if point strategy, or no close particles for point-and-connection stategy
+    if (strategy == STRATEGY_POINTS || (strategy == STRATEGY_CONNECTIONS_AND_POINTS && searchResults.size() <= 1)) {
+      newPointMesh.addVertex({ p.position, 0.0 });
+      ofFloatColor c = p.color;
+      c.a = p.color.a * colourMultiplier;
+      newPointMesh.addColor(c);
+    }
   }
+  
+  lock();
+  std::swap(newLineMesh, lineMesh);
+  std::swap(newPointMesh, pointMesh);
+  unlock();
+}
+
+void ParticleSet::draw() {
+  ofSetColor(255);
+  lock();
+  pointMesh.draw(); // TODO: in a vert shader, can set gl_PointSize to p.drawRadius * drawScale
+  lineMesh.draw();
   unlock();
 }
 
 ofParameterGroup& ParticleSet::getParameterGroup() {
   if (parameters.size() == 0) {
     parameters.setName(getParameterGroupName());
+    parameters.add(strategy);
     parameters.add(maxParticles);
     parameters.add(maxParticleAge);
     parameters.add(particleVelocityDamping);
@@ -171,6 +202,7 @@ ofParameterGroup& ParticleSet::getParameterGroup() {
     parameters.add(particleAttractionRadius);
     parameters.add(particleConnectionRadius);
     parameters.add(particleDrawRadius);
+    parameters.add(colourMultiplier);
     parameters.add(forceScale);
   }
   return parameters;
--- src/ofxParticleSet.h
+++ src/ofxParticleSet.h
@@ -56,16 +56,22 @@ public:
   void draw();
   size_t size() const { return particles.size(); };
   
+  const int STRATEGY_POINTS = 0;
+  const int STRATEGY_CONNECTIONS = 1;
+  const int STRATEGY_CONNECTIONS_AND_POINTS = 2;
+  
   std::string getParameterGroupName() const { return "Particle Set"; }
   ofParameterGroup parameters;
+  ofParameter<int> strategy { "strategy", 1, 0, 2 };
   ofParameter<int> maxParticles { "maxParticles", 250, 100, 2000 };
   ofParameter<int> maxParticleAge { "maxParticleAge", 500, 10, 1000 };
   ofParameter<float> particleVelocityDamping { "particleVelocityDamping", 0.995, 0.9, 1.0 };
   ofParameter<float> particleAttraction { "particleAttraction", -0.02, -0.2, 0.2 };
   ofParameter<float> particleAttractionRadius { "particleAttractionRadius", 0.1, 0.0, 1.0 }; // normalised
   ofParameter<float> particleConnectionRadius { "particleConnectionRadius", 0.05, 0.0, 1.0 }; // normalised
-  ofParameter<float> particleDrawRadius { "particleDrawRadius", 0.0002, 0.0, 0.05 }; // normalised
-  ofParameter<float> forceScale { "forceScale", 0.05, 0.0, 0.1 }; // normalised
+  ofParameter<float> particleDrawRadius { "particleDrawRadius", 0.001, 0.0, 0.05 }; // normalised
+  ofParameter<float> colourMultiplier { "colourMultiplier", 0.1, 0.0, 1.0 };
+  ofParameter<float> forceScale { "forceScale", 0.1, 0.0, 0.4 }; // normalised
 
   ofParameterGroup& getParameterGroup();
 
@@ -74,13 +80,15 @@ protected:
 
 private:
   float drawScale;
+  ofMesh pointMesh, lineMesh;
   
   std::vector<Particle> particles;
-  std::vector<glm::vec2> positions; // particle positions used to create the spatial index. There must be direct way but I can't make the templates work
+  std::vector<glm::vec2> positions; // particle positions used to create the spatial index. There must be a direct way but I can't make the templates work
   SpatialIndexPtrT spatialIndexPtr;
 
   void eraseDeadParticles();
   void createSpatialIndex();
+  void updateMeshes();
 
   ofThreadChannel<ParticleSetUpdate> updates;
 };
